6af2d55bd455ee444cfe1cd120ebd4992d8519e4
Alex <alexhatzis64@gmail.com>
2026-02-06 18:42:16 -0800

    add points at high/low tides after current time

--- plotter.py
+++ plotter.py
@@ -1,19 +1,48 @@
 import matplotlib.pyplot as plt
 from datetime import datetime
 from time import sleep
+import pandas as pd
 
 def find_inflections(tideData):
     trend = "none"
+    prevTideLevel = tideData.iloc[0]
+    maxima = []
+    minima = []
+    for i in range(1, len(tideData)):
+        tideLevel = tideData.iloc[i]
+        if tideLevel.iloc[0] > prevTideLevel.iloc[0]:
+            if trend == "down":
+                #found inflection
+                minima.append(i-1)#prevTideLevel.name)
+            trend = "up"
+        else:
+            if trend == "up":
+                #found inflection
+                maxima.append(i-1)#)prevTideLevel.name)
+            trend = "down"
+        prevTideLevel = tideLevel
+    return maxima, minima
+        
 
 def generate_trace(tideData):
     num_pts = len(tideData)
     x_spacing = int((num_pts - 1) // 4)
 
+    maxima, minima = find_inflections(tideData)
+
+    markedPts = []
+    for max in maxima:
+        if max >= x_spacing:
+            markedPts.append(max)
+    for min in minima:
+        if min >= x_spacing:
+            markedPts.append(min)
+
     x = tideData.index
     y = tideData
 
     fig, ax = plt.subplots()
-    ax.plot(x, y, label='tides for sg beach')
+    ax.plot(x, y, 'o', ls='-', label='tides for sg beach', markevery=markedPts)
 
     xticks =   [tideData.iloc[x_spacing*0].name, 
                 tideData.iloc[x_spacing*1].name, 
