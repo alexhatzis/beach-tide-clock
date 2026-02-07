b7fff9342eb711690e042838b2af7ee12247f06b
Alex <alexhatzis64@gmail.com>
2026-02-06 17:17:51 -0800

    rewrite noaa to be more oo

--- NOAA_tide_data.py
+++ NOAA_tide_data.py
@@ -4,49 +4,50 @@ from datetime import timedelta
 from datetime import datetime
 from datetime import time
 
+class tides_data:
+    def __init__(self, stationIds, hrsPrior, hrsFuture, dpInterval):
+        self.stations = []
+        for stationId in stationIds:
+            self.stations.append(Station(id=stationId))
+        self.hrsPrior = hrsPrior
+        self.hrsFuture = hrsFuture
+        self.dpInterval = dpInterval
+        self.waterLevels = []
 
-def get_tide_info():
+    def get_tide_info(self):
 
-    #create NOAA station objects for Point Reyes and Monterey
-    pointreyes = Station(id="9415020")
-    monterey = Station(id="9413450")
+        #create datetime date objects for the start and end of the possible tide time ranges
+        #i.e., beginning of yesterday until end of tomorrow
+        today = date.today()
+        yesterday = today - timedelta(days=1)
+        tomorrow = today + timedelta(days=2)
 
-    #create datetime date objects for the start and end of the possible tide time ranges
-    #i.e., beginning of yesterday until end of tomorrow
-    today = date.today()
-    yesterday = today - timedelta(days=1)
-    tomorrow = today + timedelta(days=2)
+        #get the current number of minutes since the start of our time interval, midnight yesterday
+        currenttime = datetime.now()
+        currentminute = (currenttime - datetime.combine(yesterday, time())).total_seconds() // 60
 
-    #get the current number of minutes since the start of our time interval, midnight yesterday
-    currenttime = datetime.now()
-    currentminute = (currenttime - datetime.combine(yesterday, time())).total_seconds() // 60
+        #string formatter for NOAA API call (i.e. YYYYMMDD)
+        datefmtstr = "%Y%m%d"
 
-    #string formatter for NOAA API call (i.e. YYYYMMDD)
-    datefmtstr = "%Y%m%d"
+        #create formatted strings for dates to use in NOAA API call
+        yesterdayfmt = yesterday.strftime(datefmtstr)
+        tomorrowfmt = tomorrow.strftime(datefmtstr)
 
-    #create formatted strings for dates to use in NOAA API call
-    yesterdayfmt = yesterday.strftime(datefmtstr)
-    tomorrowfmt = tomorrow.strftime(datefmtstr)
+        #data point corresponding to the current time, floored to last multiple of dpinterval minutes
+        curr_pt = int(currentminute // self.dpInterval)
 
-    #number of hours before and after current time to display tide information for
-    hrs_past = 6
-    hrs_future = 18
+        #data points corresponding to the beginning and end of our display interval
+        past_pt = curr_pt - (self.hrsPrior * 60 // self.dpInterval)
+        future_pt = curr_pt + (self.hrsFuture * 60 // self.dpInterval) + 1
 
-    #number of minutes between tide prediction data
-    dpinterval = 5
+        #get water level data for Point Reyes and Monterey using NOAA API call
+        for station in self.stations:
+            if len(self.waterLevels):
+                self.waterLevels += station.get_data(begin_date=yesterdayfmt, end_date=tomorrowfmt, product="predictions", interval=f'{self.dpInterval}', datum="MLLW", units="english", time_zone="lst_ldt")
+            else:
+                self.waterLevels = station.get_data(begin_date=yesterdayfmt, end_date=tomorrowfmt, product="predictions", interval=f'{self.dpInterval}', datum="MLLW", units="english", time_zone="lst_ldt")
 
-    #data point corresponding to the current time, floored to last multiple of dpinterval minutes
-    curr_pt = int(currentminute // dpinterval)
+        #average the water level data for the two stations to approximate San Gregorio beach
+        self.waterLevels = self.waterLevels / len(self.stations)
 
-    #data points corresponding to the beginning and end of our display interval
-    past_pt = curr_pt - (hrs_past * 60 // dpinterval)
-    future_pt = curr_pt + (hrs_future * 60 // dpinterval) + 1
-
-    #get water level data for Point Reyes and Monterey using NOAA API call
-    pr_water_levels = pointreyes.get_data(begin_date=yesterdayfmt, end_date=tomorrowfmt, product="predictions", interval=f'{dpinterval}', datum="MLLW", units="english", time_zone="lst_ldt")
-    mr_water_levels = monterey.get_data(begin_date=yesterdayfmt, end_date=tomorrowfmt, product="predictions", interval=f'{dpinterval}', datum="MLLW", units="english", time_zone="lst_ldt")
-
-    #average the water level data for the two stations to approximate San Gregorio beach
-    sg_water_levels = (pr_water_levels.iloc[past_pt:future_pt] + mr_water_levels[past_pt:future_pt]) / 2
-
-    return sg_water_levels
+        return self.waterLevels[past_pt:future_pt]
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from NOAA_tide_data import get_tide_info
+from NOAA_tide_data import tides_data
 from plotter import generate_trace
 
-sg_tides = get_tide_info()
-
-plot_png = generate_trace(sg_tides)
+# pointreyes, monterey
+sg_tides = tides_data(["9415020", "9413450"], 6, 18, 5)
+tide_levels = sg_tides.get_tide_info()
+plot_png = generate_trace(tide_levels)
 
