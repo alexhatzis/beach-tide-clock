1a2c5feb350e7bbb3bd49b0917a27b465f7d28fe
Alex <alexhatzis64@gmail.com>
2026-02-08 15:06:27 -0800

    tweak plot labels

--- plotter.py
+++ plotter.py
@@ -42,27 +42,37 @@ def generate_trace(tideData):
     y = tideData
 
     fig, ax = plt.subplots()
-    ax.plot(x, y, 'o', ls='-', label='tides for sg beach', markevery=markedPts)
+    ax.plot(x, y, 'o', ls='-', label='tides for sg beach', color='black', markevery=markedPts)
 
     xticks =   [tideData.iloc[x_spacing*0].name, 
                 tideData.iloc[x_spacing*1].name, 
                 tideData.iloc[x_spacing*2].name, 
                 tideData.iloc[x_spacing*3].name, 
                 tideData.iloc[x_spacing*4].name]
-    
-    xlabels = []
+
     timefmtstring = "%H:%M"
+
+    xlabels = []
     for i in range (0,5):
         label = tideData.iloc[x_spacing*i].name.strftime(timefmtstring)
         xlabels.append(label)
     ax.set_xticks(ticks=xticks,
                    labels=xlabels)
     
-
     lower_ylim = -2
     upper_ylim = 10
     ax.set_ylim(bottom=lower_ylim, top=upper_ylim)
+    ax.set_xlim(left=xticks[0], right=xticks[4])
     ax.vlines(tideData.iloc[x_spacing*1].name, lower_ylim, upper_ylim, colors='Red')
+
+    for max in maxima:
+        if max >= x_spacing:
+            label = f'{tideData.iloc[max].name.strftime(timefmtstring)} - {tideData.iloc[max].iloc[0]:.1f}ft'
+            ax.annotate(label, (x[max], y.iloc[max].iloc[0]), color='black', xytext=(-30, 5) , textcoords='offset points')
+    for min in minima:
+        if min >= x_spacing:
+            label = f'{tideData.iloc[min].name.strftime(timefmtstring)} - {tideData.iloc[min].iloc[0]:.1f}ft'
+            ax.annotate(label, (x[min], y.iloc[min].iloc[0]), color='black', xytext=(-30, -12) , textcoords='offset points')
 
     fig.patch.set_alpha(0.0)
     ax.patch.set_alpha(0.0)
