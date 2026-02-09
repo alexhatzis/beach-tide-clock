8d2ffe6fd2e0c897eaed4610de62fb8b48eaa390
Alex <alexhatzis64@gmail.com>
2026-02-08 21:34:17 -0800

    fix dir name

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import pygame as pg
 import sys
 import os
 
-picdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+picdir = os.path.join(os.path.dirname(os.path.realpath(__file__)))
 libdir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'lib')
 print(libdir)
 if os.path.exists(libdir):
